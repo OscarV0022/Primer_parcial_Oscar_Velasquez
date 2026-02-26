f2b2fa6057d1e7072116db8a1ad9874f1924807d
OscarV0022 <mundococ0012@gmail.com>
2026-02-25 22:18:42 -0600

    Añadido log para disparar Jenkins

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+
+    const timestamp = new Date().toLocaleString();
+    console.log(`[${timestamp}] --- Nueva petición de Login ---`);
+    console.log(`[${timestamp}] Usuario del sistema: Oscar`); 
+    console.log(`[${timestamp}] Intentando autenticar a: ${username}`);
+
+    if (username === 'admin' && password === '1234') {
+        console.log(`[${timestamp}] RESULTADO: Éxito`);
+        return res.json({ "success": true });
+    }
+
+    console.log(`[${timestamp}] RESULTADO: Fallido (Credenciales incorrectas)`);
+    res.status(401).json({ "success": false });
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor iniciado por: Oscar`);
+    console.log(`Fecha y Hora: ${new Date().toLocaleString()}`);
+    console.log(`Escuchando en puerto: ${PORT}`);
+
+    console.log("Validación de CI/CD");
+});
